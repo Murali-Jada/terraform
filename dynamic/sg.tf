7bcd0844d5712ede1065ee97ec31f0c93e0061b9
Murali <muralijada24@gmail.com>
2024-05-12 00:39:56 +0530

    Terraform

--- dynamic/sg.tf
+++ dynamic/sg.tf
@@ -1,25 +1,27 @@
-variable "inbound_rules" {
-    type = list
-    default = [
-        {
-            port = 22,
-            allowed_cidr = ["0.0.0.0/0"]
-            protocol         = "tcp"
-        },
-        {
-            port = 80,
-            allowed_cidr = ["0.0.0.0/0"]
-            protocol         = "tcp"
-        },
-        {
-            port = 8080,
-            allowed_cidr = ["0.0.0.0/0"]
-            protocol         = "tcp"
-        },
-        {
-            port = 3306,
-            allowed_cidr = ["0.0.0.0/0"]
-            protocol         = "tcp"
+resource "aws_security_group" "allow_ports" {
+    name = "allow_ports"
+    description = "allowing 22,80,8080,3306 access"
+
+    #terraform block
+    dynamic "ingress" {
+        for_each = var.inbound_rules
+        content{
+            from_port        = ingress.value["port"] #each.value[<key-name>]
+            to_port          = ingress.value["port"]
+            protocol         = ingress.value["protocol"]
+            cidr_blocks      = ingress.value["allowed_cidr"]
         }
-    ]
+    }
+
+    egress {
+        from_port        = 0 # from 0 to 0 means, opening all protocols
+        to_port          = 0
+        protocol         = "-1" # -1 all protocols
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "allow_ports"
+        CreatedBy = "Sivakumar"
+    }
 }
